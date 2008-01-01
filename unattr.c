e9a0b65677340aefbddeb34108b14aeb21ea8b33
Brice Arnould <brice.arnould+shake@gmail.com>
2008-01-02 00:29:09 +0100

    Refer to the online version of the GPL

--- unattr.c
+++ unattr.c
@@ -5,14 +5,14 @@
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 // TODO : move xattr_* out of this file for portability
 
